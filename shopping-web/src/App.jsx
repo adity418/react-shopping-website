a10e26e838607d368f15b3c0f3039ab46c368c46
aditya-dev <adity119@gmail.com>
2025-03-28 13:44:13 +0530

    Update modified files

--- shopping-web/src/App.jsx
+++ shopping-web/src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import HomePage from "./components/home/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetail from "./components/itemDetail/ItemDetail";
+import Navbar from "./components/navbar/Navbar";
+import Cart from "./components/cart/Cart";
+import Orders from "./components/orders/Orders";
+import Checkout from "./components/checkout/Checkout";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <div>
-        
-      </div>
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/item/:id" element={<ItemDetail />}/>
+          <Route path="/cart" element={<Cart />}/>
+          <Route path="/orders" element={<Orders />}/>
+          <Route path="/checkout" element={<Checkout />}/>
+          <Route exact path="/" element={<HomePage />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
+
